3fbc45b19a0b4dde01ac0e10dd6989b98d6c0a3e
katochanchal11 <54839491+katochanchal11@users.noreply.github.com>
2020-11-23 21:41:46 +0530

    Add files via upload

---
+++ Day1/bestenlist1task2.py
@@ -0,0 +1,17 @@
+Hours = "thirty"
+print(Hours)
+
+Challenge = "I will win"
+print(Challenge[7:10])
+print(Challenge.lower())
+print(Challenge.casefold())
+print(len(Challenge))
+
+a = "30 Days"
+b = "30 Hours"
+print(a+ " "+b)
+
+# Task 2
+a = "30 days 30 hour challenge"
+print(a[1: ])
+
