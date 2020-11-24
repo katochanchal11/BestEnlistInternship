bf01be4809c7bf3f5c5c0f0fe6125845cf090fcd
katochanchal11 <54839491+katochanchal11@users.noreply.github.com>
2020-11-24 23:12:51 +0530

    Add files via upload

---
+++ Day2/task 2.py
@@ -0,0 +1,24 @@
+#1
+a=b=c=200
+print(a,b,c)
+print("\n")
+
+a=a//10
+b=b*50
+c=c+60
+
+print(a,b,c)
+print("\n")
+
+#2
+string="Hello World"
+print(string)
+print(string.replace("W","Y"))
+print("\n")
+#this replaces every "W" in string with "Y" without affecting it
+
+#3
+a=100
+b=3.14
+print(float(a))
+print(int(b))
